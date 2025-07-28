89a4112cf44a2300a88ecea731e2fa022bc2c4ab
palak-parwani <parwanipalak3@gmail.com>
2025-07-28 13:22:43 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // Required for static export
+  images: {
+    unoptimized: true, // Required because Image Optimization is not supported in static export
+  },
+  basePath: '/palak-portfolio', // Use your GitHub repo name
+};
 
 export default nextConfig;
